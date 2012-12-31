ab356f6488a056ba5e7237d0566a17597d99ee4d
tmkctm <tzali.maimon@eqsquest.com>
2013-01-01 00:38:50 +0200

    JUnit acting up

--- src/LJava/LazyGroup.java
+++ src/LJava/LazyGroup.java
@@ -3,6 +3,7 @@ import static LJava.LJ.increment;
 import static LJava.LJ.val;
 import static LJava.LJ.var;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -120,6 +121,24 @@ public class LazyGroup extends Association {
 		sb.append(" )");
 		return sb.toString();
 	}	
+
+	
+	private class MapComparatorByValue<T> implements Comparator<T> {	
+		Map<T,Integer> sourceMap;		
+		public MapComparatorByValue(HashMap<T,Integer> m) {
+			sourceMap=m;
+		}		
+		@Override
+		public int compare(T a, T b) {
+			if (sourceMap.get(a)>sourceMap.get(b)) return -1;
+			else if (sourceMap.get(a)<sourceMap.get(b)) return 1;
+			int aHash=a.hashCode();
+			int bHash=b.hashCode();
+			if (aHash>bHash) return -1;
+			else if (aHash<bHash) return 1;
+			return 0;
+		}
+	}
 	
 }	
 
--- src/Tester.java
+++ src/Tester.java
@@ -1,377 +1,57 @@
-import java.util.LinkedList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import LJava.*;
 import static LJava.LJ.*;
 import static LJava.Utils.*;
+import static LJava.MathFormulas.*;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class Tester {
 	
-	Variable x=var();
-	Variable y=var();
-	Variable z=var();
-	Variable t=var();	
+	Variable x;		Variable y;		Variable z;		Variable t;		Variable u;		Variable v;		Variable LJvar;
+	
+	private class Container {
+		public int i;			public double d;
+		public String s;		public List<Object> l;
+		public boolean b;		public Variable v;
+		public Constraint c;	public Relation r;
+		public Group g;			public LazyGroup lg;
+		public Object[] arr;
 		
-	@SuppressWarnings("unused")
-	private class TesterLittleHelper{		
-		 private Integer value;
-		 public void set(int v) {  value=v;  } 
-		 public int get() {  return value;  }
-		 public String toString(){ 
-			 if (value==null) return "Helper";
-			 return value.toString(); }
+		public String toString() {
+			return "int: "+i+" ; double: "+d+" ; String: "+s+" ; List: "+l+"\n"
+					+"boolean: "+b+" ; Variable: "+v+" ; Constraint: "+c+"\n"+
+					"Relation: "+r+" ; Group: "+g+" ; LazyGroup: "+lg+" ; Array: "+arr;
+		}
 	}
 	
 	
 	@Before
-	public void init(){
-		x=new Variable();
-		y=new Variable();
-		z=new Variable();
-		t=new Variable();
+	public void resetVars(){
+		x=new Variable("x");	y=new Variable("y");
+		z=new Variable("z");	t=new Variable("t");
+		u=new Variable("u");	v=new Variable("v");
+		LJvar=var();
 	}
 	
 	
-	@Test 
-	public void checkString() {
-		x.set(1,2,3,4,5,x);
-		y.set("a","b","c",_,undefined,none,LJTrue, LJFalse);
-		Relation r=new Relation("try",1,2,3);
-		assertEquals(x.toString(),"[1,2,3,4,5,$undefined$()]");
-		assertEquals(y.toString(),"[a,b,c,_,$undefined$(),$no_variable_value$(),$LJava_True$(),$LJava_False$()]");
-		assertEquals(r.toString(),"try(1,2,3)");
-	}
-	
-	
+//Test classes	
 	@Test
-	public void testNoVars(){
-		TesterLittleHelper aClass=new TesterLittleHelper();
-		relate(0,2,3,"a","b","c",aClass,50);
-		exists(0,2,3,"a",_,"c",_,50);
-	}	
-	
-	
-	@Test
-	public void testOnVars(){
-		TesterLittleHelper aClass=new TesterLittleHelper();
-		relate(1,2,3,"a","b","c", aClass, z);
-		exists(1,2,3,_,_,_,x,y);
-		assertEquals(x,aClass);
-		assertEquals(y,z);
+	public void testAssociation() {
+		Association a=new Association("");
+		assertEquals(a.name(),"#LJRelation");
+		Container container=new Container();
+		container.b=false;
+		container.l=new ArrayList<Object>();		container.l.add(x);		container.l.add("tzali");		container.l.add(10);
+		container.v=var();		container.v.set(1,2,3);
+		y.set(11,12,13);
+		a=new Association("a", 0, container, false, new HashSet(), LJvar, x, y);
+		System.out.println(a);
 	}
-	
-	
-	@Test
-	public void testOneVarMultiple(){
-		relate(2,10,20,30, y, y);
-		exists(2,x,x,_,_,_);
-		assertTrue(var(x));		
-		exists(2,10,20,_,z,z);
-		assertFalse(var(z));
-		y.set(100);
-		assertEquals(z,100);
-	}
-	
-	
-	public void testVariableChain(){
-		relate(3,1,2,z);
-		relate(3,"A",y,"C");
-		exists(3,1,2,y);
-		exists(3,"A",x,_);
-		exists(3,z,2,_);
-		assertEquals(x,1);		
-	}
-	
-		
-	@Test
-	public void testAll(){
-		relate(4,z,y,0);
-		relate(4,1,2,3);
-		relate(4,"a","b","c");
-		Relation r=relation("test");
-		all(4,_,x,_);
-		y.set("t");
-		assertEquals(x,"t");
-		Variable c=new Variable();
-		Variable d=new Variable();
-		c.set("t",2,"b");
-		d.set(2,"t","b");
-		assertTrue(x.equalValues(c));
-		assertFalse(x.equalValues(d));
-		assertTrue(x.equalValuesSet(d));
-		assertEquals(all(r),false);
-		associate(r);
-		assertEquals(all(r),true);
-		assertEquals(all(),false);
-		assertEquals(all(4,1,2,3),true);
-		assertEquals(all("t","m",2,3),false);
-	}
-	
 
-	@Test
-	public void testConsistencyForSingleVar(){
-		relate(12,100,200,x);
-		exists(12,100,200,x);
-		assertFalse(var(x));
-		assertEquals(x,undefined);
-	}
-	
-	
-	@Test
-	public void testConsistencyForeTwoVars(){
-		relate(5,100,200,x);
-		exists(5,100,200,y);
-		relate(5,y,6,6);
-		exists(5,x,6,6);
-		assertFalse(var(y));
-		assertFalse(var(x));
-		x.set(30);
-		assertEquals(x,y);
-		assertEquals(y,undefined);
-	}
-	
-	
-	@Test
-	public void testConsistencyForeVarsChain(){
-		relate(6,0,0,y);
-		relate(6,z,1,1);
-		relate(6,x,5,5);
-		relate(6,3,t,3);
-		exists(6,y,5,5);
-		exists(6,0,0,z);
-		exists(6,t,1,1);
-		exists(6,3,x,3);
-		Variable a = var();
-		exists(6,a,5,5);
-		assertEquals(x,z);
-		assertEquals(y,t);
-		assertEquals(x,undefined);
-		assertEquals(a, undefined);
-	}
-	
-	
-	@Test
-	public void testConsistencyForCunduct(){
-		TesterLittleHelper helper=new TesterLittleHelper();
-		helper.set(29);
-		relate(7,y,y,0,1);
-		relate(7,1,1,30,1);
-		relate(7,"a","a","b",1);
-		relate(7,helper,helper,1,1);
-		relate(7,700,0,700,1);
-		relate(7,"Some","Stuff",1);
-		relate(7,"Some","more","Stuff",1);
-		relate(7,7,8,x);		
-		all(7,x,x,_,_);
-		exists(7,7,8,y);	
-		Variable c=new Variable();
-		c.set(y,1,"a",helper);
-		assertTrue(x.equalValues(c));
-		assertEquals(x,undefined);
-	}
-	
-	
-	@Test
-	public void testORop(){
-		relate(8,1,"a");
-		relate(8,2,"b");
-		relate(8,3,"c");
-		all(relation(8,x,_),OR,relation(8,_,x));
-		assertEquals(x,1);
-		Variable c=new Variable();
-		c.set(1,2,3,"a","b","c");
-		assertTrue(x.equalValues(c));
-		x=var();
-		assertTrue(all(relation(8,2,x),OR,relation(8,y,x)));
-		assertTrue(x.toString().equals("[b,a,b,c]"));
-		assertTrue(y.toString().equals("[1,2,3]"));
-		group(8,80,800);
-		all(relation(800,t,z),OR,relation(8,t,"a"));
-		assertTrue(z.contains(8));
-		assertTrue(z.contains(80));
-		assertTrue(t.contains(8));
-		assertTrue(t.contains(80));
-		assertTrue(t.contains(1));
-		assertTrue((z.toString().equals("[8,80]") && t.toString().equals("[80,8,1]"))
-				   || (z.toString().equals("[80,8]") && t.toString().equals("[8,80,1]")));
-	}
-	
-	
-	@Test
-	public void testANDop(){
-		relate(9,1,"a");
-		relate(9,2,3);
-		relate(9,3,"c");
-		all( and( relation(9,x,_) , relation(9,_,x) ));
-		assertEquals(x,3);
-		assertFalse(x.isConstraint());
-		assertTrue(x.isNumber());		
-	}
-	
-	
-	@Test
-	public void testLogicAll(){
-		TesterLittleHelper helper=new TesterLittleHelper();
-		helper.set(1000);
-		relate(10,1,2,3,4,5,6);
-		relate(10,70);
-		relate(10,6);
-		relate(10,0,9,8,7,6,5,4,3,2,helper);
-		relate(10,700,9,8,7,1600,5,4,3,2,1);
-		relate(10,helper,10,helper,10,helper);
-		relate(10,0,10,0,10,0);
-		all( or(
-				and(
-					relation(10,1,2,3,4,5,x),
-					relation(10,x)
-				),
-				and(
-					relation(10,x,9,8,7,_,5,4,3,2,y),
-					relation(10,y,10,y,10,y)
-				)	
-			));
-		assertTrue(x.isConstraint());
-		assertFalse(y.isConstraint());
-		assertEquals(x,6);
-		assertEquals(y,helper);
-	}
-	
-	
-	@Test
-	public void testLogicAllNoVars(){
-		relate(11,1,2,3,4,5,6);
-		relate(11,70);
-		relate(11,6);
-		relate(11,0,11,0,11,0);
-		assertEquals(all( or(
-							and(
-								relation(11,1,2,3,4,5,6),
-								relation(11,6)
-							),
-							and(
-								relation(11,_,9,8,7,_,5,4,3,2,0),
-								relation(11,1,11,1,11,2)
-							)	
-					))
-					,true);
-		
-	}
-	
-
-	@Test
-	public void testGroup(){
-		group(12,2,3,4,5,6,7,8,9,0);
-		group(12,2,3,4,5,x,x,y);
-		group(12,12,13,13,x,y,z);
-		group(12,12,12,12,12,13);
-		group(12,12,12,12,12,13,13,13,13,13);
-		group(12,12,14,13,13);
-		group(12,12,12,12,300,12,100,200,100,12);
-		assertFalse(exists(12,2,3,4,5,t,t,t));
-		assertTrue(exists(12,2,3,4,5,x,x,y));
-		assertTrue(var(x));
-		exists(12,3,y,5,6,2,7,8,9,4);
-		exists(12,5,4,z,6,2,7,8,9,y);
-		assertEquals(y,0);
-		assertEquals(z,3);
-		assertTrue(exists(12,12,0,3,13,13,x));
-		x.set(1);
-		assertTrue(exists(12,2,3,4,5,x,x,y));
-		assertTrue(exists(0,2,1,4,5,3,1,12));
-		x=var();
-		assertTrue(exists(x,x,t,x,x,x));
-		assertEquals(x,12);
-		assertEquals(t,13);	
-		x=var();   t=var();
-		assertFalse(exists(x,x,t,x,t));
-		assertTrue(var(x));
-		assertTrue(var(t));
-		exists(x,t,x,t,x,t,x,x,t,t);
-		assertTrue(((Integer) x.get()==12) || ((Integer) x.get()==13));
-		assertTrue(((Integer) t.get()==12) || ((Integer) t.get()==13));
-		x=var();     y=var();     z=var();
-		assertTrue(exists(x,y,x,y,z));
-		assertTrue(x.contains(12));		assertTrue(x.contains(13));
-		assertTrue(y.contains(13));		assertTrue(y.contains(12));
-		assertFalse(z.contains(13));		assertTrue(z.contains(14));
-		assertTrue((x.toString().equals("[12,13]") && y.toString().equals("[13,12]")) || (x.toString().equals("[13,12]") && y.toString().equals("[12,13]")));
-		assertEquals(z.toString(), "[14,14]");
-		x=var();     y=var();     z=var();	t=var();
-		assertTrue(exists(t,x,y,x,x,x,y,x,z,x));
-		assertEquals(x.toString(), "[12,12]");
-		assertEquals(y.toString(), "[100,100]");
-		assertTrue((z.toString().equals("[300,200]") && t.toString().equals("[200,300]")) || (z.toString().equals("[200,300]") && t.toString().equals("[300,200]")));
-	}
-	
-	
-	@Test
-	public void testFormulas() {
-		Formula<Integer,Integer> sum=new Formula<Integer,Integer>("Sum", Integer.class){
-			@Override
-			protected Integer f(Integer... p) {
-				return p[0]+p[1];
-			}};
-		
-		assertEquals(exists(sum),false);
-		assertEquals(exists(cmp),false);
-		associate(sum);
-		assertEquals(exists(sum),true);
-		exists(relation("Sum",t,4,5));
-		assertTrue(same(t,9));
-		relate(13,1,2,3);
-		group(13,11,12,14,200,201,202);
-		Variable a = var();
-		all(relation("Sum",z,a,10));
-		assertFalse(var(z));
-		assertTrue(var(a));
-		assertFalse(z.contains(10000));
-		assertFalse(a.contains(10000));
-		assertTrue(a.set(100));
-		assertTrue(z.contains(110));
-		all(relation(x,12,14,11,202,200,201),AND,relation("Sum",x,4,1));	
-		assertFalse(same(x.toString(),none.toString()));
-		assertTrue(var(x));
-		all(relation(y,12,14,11,202,200,201),OR,relation("Sum",y,12,15));		
-		assertTrue(same(y,13));
-		assertTrue(y.contains(27));
-		exists(relation("Sum",x,y,7));
-		assertTrue(same(x,20));
-	}
-	
-	
-	@Test
-	public void testConstraint() {
-		Integer[] arr = {1,2,3,4,5,6,7,8,9,0};
-		Constraint c1 = new Constraint(cmp,1,x,y);
-		Constraint c2 = new Constraint(cmp,-1,x,4);
-		Constraint c3 = new Constraint(cmp,0,x,9);
-		Constraint c = new Constraint(new Constraint(c1,AND,c2),OR, c3);
-		assertEquals(c.toString(),"((Compare(1,[$LJ_Variable$],[$LJ_Variable$])) AND (Compare(-1,[$LJ_Variable$],4))) OR (Compare(0,[$LJ_Variable$],9))");
-		assertFalse(c1.satisfy(x,400));
-		assertTrue(c2.satisfy(x,-400));
-		Variable[] vs = {x,y};
-		Object[] os = {1400,400};
-		assertTrue(var(x));
-		assertTrue(var(y));
-		assertTrue(c1.satisfy(vs,os));
-		assertTrue(c1.satisfy(x,1400,y,400));
-		y.set(1);
-		assertTrue(c1.satisfy(x,400));
-		assertTrue(x.instantiate(arr, c, new Constraint(LJFalse)));
-		assertEquals(x.toString(),"[2,3,9]");
-		assertTrue(x.isConstraint());
-		assertTrue(x.contains(3));
-		assertTrue(x.contains(9));
-		assertFalse(x.contains(1));
-		Variable v=var();
-		Constraint vConstraint=c.replaceVariable(x, v);
-		v.instantiate(new LinkedList<Object>(), vConstraint, new Constraint(cmp,0,v,50));		
-		assertFalse(v.contains(90));
-		assertFalse(v.contains(0));
-		assertTrue(v.contains(50));
-		assertFalse(v.contains(5));
-		assertEquals(v.toString(),"[] OR Compare(0,[$LJ_Variable$],50)");
-	}
 }
